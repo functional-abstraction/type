647b0dcccaee836be039edb9a7919effbb24716b
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:39 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/type.js
+++ js/dist/type.js
@@ -199,10 +199,10 @@ if ( typeof exports === "object" ) {
 	definition( exports ) ;
 }
 else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-type" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+	define( "@aureooms/js-type" , [ ] , function ( ) { return definition( { } ) ; } ) ;
 }
 else if ( typeof window === "object" && typeof window.document === "object" ) {
 	definition( window["type"] = { } ) ;
 }
-else console.error( "unable to detect type of module to define for aureooms-js-type") ;
+else console.error( "unable to detect type of module to define for @aureooms/js-type") ;
 } )( ) ;
